769a12e9b605bb04c2d421e3b55c767a2134f4fb
oli <oli.millington@gmail.com>
2024-04-20 09:42:58 +0700

    pre commit to test which files change on cypress config

---
+++ apps/example/cypress/e2e/app.cy.ts
@@ -0,0 +1,7 @@
+describe('example', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
+});
---
+++ apps/example/cypress/support/app.po.ts
@@ -0,0 +1,2 @@
+// -- Example
+// export const getGreeting = () => cy.get('h1');
